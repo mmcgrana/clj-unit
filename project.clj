3a9184360cc4bf8ac017a2a16de88a7577bfa281
Mark McGranaghan <mmcgrana@gmail.com>
2009-12-24 05:20:55 -0500

    Leiningen build scheme, updated README.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-unit "0.1.0-SNAPSHOT"
+  :description "A simple unit testing library for Clojure."
+  :url "http://github.com/mmcgrana/clj-unit"
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [clj-stacktrace "0.1.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
