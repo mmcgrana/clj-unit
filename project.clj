1b32def46c8b50ee6bbe37f431953b50aca64fc6
Mark McGranaghan <mmcgrana@gmail.com>
2010-03-06 11:10:35 -0500

    Release version 0.1.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-unit "0.1.0-SNAPSHOT"
+(defproject clj-unit "0.1.0"
   :description "A simple unit testing library for Clojure."
   :url "http://github.com/mmcgrana/clj-unit"
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [clj-stacktrace "0.1.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [clj-stacktrace "0.1.0"]]
+  :dev-dependencies [[org.clojars.mmcgrana/lein-clojars "0.5.0"]])
