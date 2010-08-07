b9e23f30b2d26c7169d32e4bbd220929caa53952
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-07 13:50:16 -0700

    Update dependencies for Clojure 1.2.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject clj-unit "0.1.0"
   :description "A simple unit testing library for Clojure."
   :url "http://github.com/mmcgrana/clj-unit"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [clj-stacktrace "0.1.0"]]
-  :dev-dependencies [[org.clojars.mmcgrana/lein-clojars "0.5.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0-RC2"]
+                 [org.clojure/clojure-contrib "1.2.0-RC2"]
+                 [clj-stacktrace "0.1.3"]])
